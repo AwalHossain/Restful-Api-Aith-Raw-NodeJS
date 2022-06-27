26b7a67e73aaa8518369893cc4e0fd6710915763
AwalHossain <waqin21@gmail.com>
2022-06-27 19:56:18 +0600

    http get method tested

--- index.js
+++ index.js
@@ -5,20 +5,34 @@
 */
 
 // Dependencies
+"use strict"
+
 const http = require('http');
 const url = require('url');
 
 
 
-const server = http.createServer();
+const server = http.createServer((req,res)=>{
+ // Parse the url
+  var parsedUrl = url.parse(req.url, true);
 
-server.on('request' , (req,res)=>{
-    const parsedUrl = url.parse(req.url);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g,"");
-    console.log(trimmedPath,"ul");
-    res.end("Sever is started with raw nodejs");
-})
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+//   get the http method
+
+    var method = req.method.toLowerCase();
+
+
+  // Send the response
+  res.end('Hello World!\n');
+
+  // Log the request/response
+  console.log('Request received on path: '+trimmedPath+ " method "+method);
+});
+
+// server.on('request' , )
 
 
 server.listen(5000, ()=>{
