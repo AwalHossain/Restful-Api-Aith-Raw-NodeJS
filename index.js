06fdecfb76a0660e5e3314a2609ab4f552f2bd8b
AwalHossain <waqin21@gmail.com>
2022-06-27 18:56:03 +0600

    url trimmed with regex

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+/*
+*
+===Primary file for the Api
+*
+*/
+
+// Dependencies
 const http = require('http');
+const url = require('url');
 
 
 
 const server = http.createServer();
 
 server.on('request' , (req,res)=>{
-    res.statusCode  = 200;
-    res.setHeader('Content-type', "text/plain")
+    const parsedUrl = url.parse(req.url);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,"");
+    console.log(trimmedPath,"ul");
     res.end("Sever is started with raw nodejs");
 })
 
 
-server.listen(3000, ()=>{
+server.listen(5000, ()=>{
 console.log("the serving is running on...");
 })
