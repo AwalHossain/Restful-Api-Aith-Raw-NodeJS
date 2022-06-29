5ce4751f956c813cad74bc5417899e86b636d98c
AwalHossain <waqin21@gmail.com>
2022-06-29 18:13:22 +0600

    node decoder and buffer method added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 const http = require('http');
 const url = require('url');
-
+var {StringDecoder} = require('string_decoder');
 
 
 const server = http.createServer((req,res)=>{
@@ -23,14 +23,32 @@ const server = http.createServer((req,res)=>{
 
     var method = req.method.toLowerCase();
 
+    // get the payload, if any
 
-    // get the headers as an object
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+
+
+    req.on('data', function(data){
+      buffer += decoder.write(data)
+    })
+
+    req.on('end', ()=>{
+
+      buffer += decoder.end();
     var headers = req.headers;
 
-  console.log(headers , "pars");
-  // Send the response
-  res.end('Hello World!\n');
+    // Send the response
+    res.end('Hello World!\n');
+    console.log(buffer , "pars");
 
+
+    })
+
+
+
+    // get the headers as an object
   // Log the request/response
   // console.log(`so the headers is ${headers}`);
 });
