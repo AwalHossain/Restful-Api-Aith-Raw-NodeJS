dbe08b6560b4777d62d2f040d9d11641ead9c277
AwalHossain <waqin21@gmail.com>
2022-07-07 14:47:27 +0600

    modified config file 😀

--- config.js
+++ config.js
@@ -1,27 +1,31 @@
 
 
-var environment = {};
-
+const environment = {};
 
 environment.stagging = {
     "port": 3000,
-    "envName":"staging"
+    "envName": "stagging"
+
 }
 
 environment.production = {
-    
     "port": 5000,
-    "envName":"production"
+    "envName": "production"
 }
 
 
-//  Determine which environment was passed as command line
 
-var currenntEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : '';
+//  DETERMINE WHICH ENVIRONMENT WAS PASSED AS COMMAND LINE
+
+let currenEnv = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : "";
+
 
 //check the current environment is one of the environment above, if so added or put a defaul one
 
- var environmentToExport = typeof(environment[currenntEnvironment]) === 'object' ? environment[currenntEnvironment] : environment.stagging;
+let environmentToExport = typeof(environment[currenEnv]) === 'object' ? environment[currenEnv] : environment.stagging;
+
+
+
 
 
 
