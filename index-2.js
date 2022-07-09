27fc47fe2ccf1f8335c8e67bcb8f1dbb988cd830
AwalHossain <waqin21@gmail.com>
2022-07-09 18:53:18 +0600

    route method added into event

--- index-2.js
+++ index-2.js
@@ -3,73 +3,91 @@
 
 const http = require('http');
 const url = require('url')
-const PORT = 5000; 
+const PORT = 5000;
+const { StringDecoder } = require('string_decoder')
 
 
 
 
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
+
+    unified(req, res)
+
+});
+
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server is listenning throug ${PORT}`);
+})
+
+
+
+// reqest container 
+
+const unified = function (req, res) {
+
     const parsedUrl = url.parse(req.url);
     const queryStringObject = parsedUrl.query;
     const pathName = parsedUrl.pathname;
     const trimmedPath = pathName.replace(/^\/+|\/+$/g, '');
     const method = req.method;
     const headers = req.headers;
-    
-    
-    const checkPath = typeof(router[trimmedPath]) !== "undefined" ? router[trimmedPath] : handler.notFound;
-    const data = {
-        "trimmedPath":trimmedPath,
-        "queryStringObject": queryStringObject,
-        "method": method,
-        "headers": headers
-        
-    }
-    // console.log(handler.notFound(),"path");
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data)
+    })
 
-    checkPath(data, function(statusCode, paylaod){
 
-        statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+    req.on('end', () => {
 
-        paylaod = typeof(paylaod) === 'object' ? paylaod : {};
+        buffer += decoder.end();
 
-        const strinObject = JSON.stringify(paylaod);
+        const checkPath = typeof (router[trimmedPath]) !== "undefined" ? router[trimmedPath] : handler.notFound;
+        const data = {
+            "trimmedPath": trimmedPath,
+            "queryStringObject": queryStringObject,
+            "method": method,
+            "headers": headers
 
-        res.writeHead(statusCode).end(strinObject)
+        }
+        // console.log(handler.notFound(),"path");
+
+        checkPath(data, function (statusCode, paylaod) {
+
+
+            statusCode = typeof (statusCode) === 'number' ? statusCode : 200;
+
+            paylaod = typeof (paylaod) === 'object' ? paylaod : {};
+
+            const strinObject = JSON.stringify(paylaod);
+
+            res.writeHead(statusCode).end(strinObject)
+
+        })
+
 
     })
 
 
-
-
-    // res.writeHead(200).end("server is working")
-    // unified(req, res)
-
-});
-
-
-const unified = function(req, res){
-
 }
-
-
-server.listen(PORT, ()=>{
-    console.log(`Server is listenning throug ${PORT}`);
-})
 
 
 const handler = {};
 
-handler.sample = function (data, callBack){
+handler.sample = function (data, callBack) {
 
-    callBack(200, {'stat':"Yes, You made it"})
+    callBack(200, { 'stat': "Yes, You made it" })
 }
-handler.notFound = function(data, callBack){
-    callBack(500, {'stat':"Yes, You madesssss it"})
+handler.notFound = function (data, callBack) {
+    callBack(500, { 'stat': "Yes, You madesssss it" })
 }
 
 const router = {
     "sample": handler.sample,
-    "not" : handler.notFound,
+    "not": handler.notFound,
 }
