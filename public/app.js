ae7f3776b11a7e371796069258ff507ec5d79155
Awal hossain <waqin21@gmail.com>
2022-08-06 20:13:11 +0600

    login session created 👍

--- public/app.js
+++ public/app.js
@@ -289,36 +289,37 @@ app.setSessionToken = function(token){
 // Renew the token
 app.renewToken = function(callback){
   var currentToken = typeof(app.config.sessionToken) == 'object' ? app.config.sessionToken : false;
-  if(currentToken){
-    // Update the token with a new expiration
-    var payload = {
-      'id' : currentToken.id,
-      'extend' : true,
-    };
-    app.client.request(undefined,'api/token','PUT',undefined,payload,function(statusCode,responsePayload){
-      // Display an error on the form if needed
-      if(statusCode == 200){
-        // Get the new token details
-        var queryStringObject = {'id' : currentToken.id};
-        app.client.request(undefined,'api/token','GET',queryStringObject,undefined,function(statusCode,responsePayload){
-          // Display an error on the form if needed
-          if(statusCode == 200){
-            app.setSessionToken(responsePayload);
-            callback(false);
-          } else {
-            app.setSessionToken(false);
-            callback(true);
-          }
-        });
-      } else {
-        app.setSessionToken(false);
-        callback(true);
-      }
-    });
-  } else {
-    app.setSessionToken(false);
-    callback(true);
-  }
+  console.log( "From test", app.config);
+  // if(currentToken){
+  //   // Update the token with a new expiration
+  //   var payload = {
+  //     'id' : currentToken.id,
+  //     'extend' : true,
+  //   };
+  //   app.client.request(undefined,'api/token','PUT',undefined,payload,function(statusCode,responsePayload){
+  //     // Display an error on the form if needed
+  //     if(statusCode == 200){
+  //       // Get the new token details
+  //       var queryStringObject = {'id' : currentToken.id};
+  //       app.client.request(undefined,'api/token','GET',queryStringObject,undefined,function(statusCode,responsePayload){
+  //         // Display an error on the form if needed
+  //         if(statusCode == 200){
+  //           // app.setSessionToken(responsePayload);
+  //           // callback(false);
+  //         } else {
+  //           // app.setSessionToken(false);
+  //           // callback(true);
+  //         }
+  //       });
+  //     } else {
+  //       app.setSessionToken(false);
+  //       callback(true);
+  //     }
+  //   });
+  // } else {
+  //   app.setSessionToken(false);
+  //   callback(true);
+  // }
 };
 
 // Loop to renew token often
