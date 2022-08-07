ab50d2fb28ff74b7d8dce9d13480808db6b007e1
Awal hossain <waqin21@gmail.com>
2022-08-07 16:01:39 +0600

    delete token method included 👍

--- public/app.js
+++ public/app.js
@@ -8,36 +8,36 @@ var app = {};
 
 // Config
 app.config = {
-  'sessionToken' : false
+  'sessionToken': false
 };
 
 // AJAX Client (for RESTful API)
 app.client = {}
 
 // Interface for making API calls
-app.client.request = function(headers,path,method,queryStringObject,payload,callback){
+app.client.request = function (headers, path, method, queryStringObject, payload, callback) {
 
   // Set defaults
-  headers = typeof(headers) == 'object' && headers !== null ? headers : {};
-  path = typeof(path) == 'string' ? path : '/';
-  method = typeof(method) == 'string' && ['POST','GET','PUT','DELETE'].indexOf(method.toUpperCase()) > -1 ? method.toUpperCase() : 'GET';
-  queryStringObject = typeof(queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {};
-  payload = typeof(payload) == 'object' && payload !== null ? payload : {};
-  callback = typeof(callback) == 'function' ? callback : false;
+  headers = typeof (headers) == 'object' && headers !== null ? headers : {};
+  path = typeof (path) == 'string' ? path : '/';
+  method = typeof (method) == 'string' && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method.toUpperCase()) > -1 ? method.toUpperCase() : 'GET';
+  queryStringObject = typeof (queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {};
+  payload = typeof (payload) == 'object' && payload !== null ? payload : {};
+  callback = typeof (callback) == 'function' ? callback : false;
 
   // For each query string parameter sent, add it to the path
-  var requestUrl = path+'?';
+  var requestUrl = path + '?';
   var counter = 0;
-  for(var queryKey in queryStringObject){
-     if(queryStringObject.hasOwnProperty(queryKey)){
-       counter++;
-       // If at least one query string parameter has already been added, preprend new ones with an ampersand
-       if(counter > 1){
-         requestUrl+='&';
-       }
-       // Add the key and value
-       requestUrl+=queryKey+'='+queryStringObject[queryKey];
-     }
+  for (var queryKey in queryStringObject) {
+    if (queryStringObject.hasOwnProperty(queryKey)) {
+      counter++;
+      // If at least one query string parameter has already been added, preprend new ones with an ampersand
+      if (counter > 1) {
+        requestUrl += '&';
+      }
+      // Add the key and value
+      requestUrl += queryKey + '=' + queryStringObject[queryKey];
+    }
   }
 
   // Form the http request as a JSON type
@@ -46,34 +46,34 @@ app.client.request = function(headers,path,method,queryStringObject,payload,call
   xhr.setRequestHeader("Content-type", "application/json");
 
   // For each header sent, add it to the request
-  for(var headerKey in headers){
-     if(headers.hasOwnProperty(headerKey)){
-       xhr.setRequestHeader(headerKey, headers[headerKey]);
-     }
+  for (var headerKey in headers) {
+    if (headers.hasOwnProperty(headerKey)) {
+      xhr.setRequestHeader(headerKey, headers[headerKey]);
+    }
   }
 
   // If there is a current session token set, add that as a header
-  if(app.config.sessionToken){
+  if (app.config.sessionToken) {
     xhr.setRequestHeader("token", app.config.sessionToken.id);
   }
 
   // When the request comes back, handle the response
-  xhr.onreadystatechange = function() {
-      if(xhr.readyState == XMLHttpRequest.DONE) {
-        var statusCode = xhr.status;
-        var responseReturned = xhr.responseText;
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      var statusCode = xhr.status;
+      var responseReturned = xhr.responseText;
 
-        // Callback if requested
-        if(callback){
-          try{
-            var parsedResponse = JSON.parse(responseReturned);
-            callback(statusCode,parsedResponse);
-          } catch(e){
-            callback(statusCode,false);
-          }
-
+      // Callback if requested
+      if (callback) {
+        try {
+          var parsedResponse = JSON.parse(responseReturned);
+          callback(statusCode, parsedResponse);
+        } catch (e) {
+          callback(statusCode, false);
         }
+
       }
+    }
   }
 
   // Send the payload as JSON
@@ -82,43 +82,74 @@ app.client.request = function(headers,path,method,queryStringObject,payload,call
 
 };
 
+// Bind the logout button
+
+app.bindLogoutButton = ()=>{
+  document.getElementById("logoutButton").addEventListener("click",(e)=>{
+    // Stop it from redirecting anywhere
+    e.preventDefault();
+
+    // logs the user out
+    app.logUserOut();
+  })
+}
+
+
+// Log the user out then redirect them
+app.logUserOut=(redirectUser)=>{
+  // set the ridirectUser to default to true
+  redirect = typeof(redirectUser) == 'boolean' ? redirectUser : true;
+
+
+  // get the current token id ;
+  let tokenId = typeof(app.config.sessionToken.id) === 'string' ? app.config.sessionToken.id : false;
+
+  // fetch the delete api
+
+  fetch(`http://localhost:3000/api/token?id=${tokenId}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data,"delete token");
+
+    if(typeof(app.config.sessionToken) !== 'object'){
+      window.location = '/session/deleted'
+    }
+  })
+}
+
+
+
+
 // Bind the forms
-app.bindForms = function(){
-    document.querySelector("form").addEventListener("submit", function(e){
-  
-      // Stop it from submitting
-      e.preventDefault();
-      var formId = this.id;
-      var path = this.action;
-      var method = this.method.toUpperCase();
-  
-      // Hide the error message (if it's currently shown due to a previous error)
-      document.querySelector("#"+formId+" .formError").style.display = 'hidden';
-  
-      // Turn the inputs into a payload
-      var payload = {};
-      var elements = this.elements;
-      for(var i = 0; i < elements.length; i++){
-        if(elements[i].type !== 'submit'){
-          var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
-          payload[elements[i].name] = valueOfElement;
-        }
+app.bindForms = function () {
+  document.querySelector("form").addEventListener("submit", function (e) {
+
+    // Stop it from submitting
+    e.preventDefault();
+    var formId = this.id;
+    var path = this.action;
+    var method = this.method.toUpperCase();
+
+    // Hide the error message (if it's currently shown due to a previous error)
+    document.querySelector("#" + formId + " .formError").style.display = 'hidden';
+
+    // Turn the inputs into a payload
+    var payload = {};
+    var elements = this.elements;
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].type !== 'submit') {
+        var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
+        payload[elements[i].name] = valueOfElement;
       }
-
-
-      // call the api
-
-      
-  
- 
-    });
-  };
+    }
+  });
+};
 
 
 // Bind the forms
-app.bindForms = function(){
-  if(document.querySelector("form")){
-    document.querySelector("form").addEventListener("submit", function(e){
+app.bindForms = function () {
+  if (document.querySelector("form")) {
+    document.querySelector("form").addEventListener("submit", function (e) {
 
       // Stop it from submitting
       e.preventDefault();
@@ -127,112 +158,76 @@ app.bindForms = function(){
       var method = this.method.toUpperCase();
 
       // Hide the error message (if it's currently shown due to a previous error)
-      document.querySelector("#"+formId+" .formError").style.display = 'hidden';
+      document.querySelector("#" + formId + " .formError").style.display = 'hidden';
 
       // Turn the inputs into a payload
       var payload = {};
       var elements = this.elements;
-      for(var i = 0; i < elements.length; i++){
-        if(elements[i].type !== 'submit'){
+      for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type !== 'submit') {
           var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
           payload[elements[i].name] = valueOfElement;
         }
       }
 
       // Call the API
-      // app.client.request(undefined,path,method,undefined,payload,function(statusCode,responsePayload){
-      //   // Display an error on the form if needed
-      //   if(statusCode !== 200){
-
-      //     // Try to get the error from the api, or set a default error message
-      //     var error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
-
-      //     // Set the formError field with the error text
-      //     document.querySelector("#"+formId+" .formError").innerHTML = error;
-
-      //     // Show (unhide) the form error field on the form
-      //     document.querySelector("#"+formId+" .formError").style.display = 'block';
-
-      //   } else {
-      //     // If successful, send to form response processor
-      //     app.formResponseProcessor(formId,payload,responsePayload);
-      //   }
-
-      // });
-
-      if(formId == 'accountCreate'){
-        fetch("http://localhost:3000/api/users",{
+      if (formId == 'accountCreate') {
+        fetch("http://localhost:3000/api/users", {
           method: 'POST',
-          headers:{
+          headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(payload)
         })
-        .then(res => res.json())
-        .then(responsePayload =>{
-          console.log(responsePayload.status === 200)
-          // If successful, send to form response processor
-          app.formResponseProcessor(formId,payload,responsePayload)
-        }
-        )
-
+          .then(res => res.json())
+          .then(responsePayload => {
+            console.log(responsePayload.status === 200)
+            // If successful, send to form response processor
+            app.formResponseProcessor(formId, payload, responsePayload)
+          }
+          )
       }
 
-      if(formId == 'sessionCreate'){
-        console.log("ofoal");
-        app.formResponseProcessor( formId,payload)
+      if (formId == 'sessionCreate') {
+        app.formResponseProcessor(formId, payload)
       }
 
-     
+
 
     });
   }
 };
 
 // Form response processor
-app.formResponseProcessor = function(formId,requestPayload){
+app.formResponseProcessor = function (formId, requestPayload) {
   var functionToCall = false;
   // If account creation was successful, try to immediately log the user in
-  if(formId == 'accountCreate' || formId == 'sessionCreate' ){
+  if (formId == 'accountCreate' || formId == 'sessionCreate') {
     // Take the phone and password, and use it to log the user in
     var newPayload = {
-      'phone' : requestPayload.phone,
-      'password' : requestPayload.password
+      'phone': requestPayload.phone,
+      'password': requestPayload.password
     };
-    // app.client.request(undefined,'api/tokens','POST',undefined,newPayload,function(newStatusCode,newResponsePayload){
-    //   // Display an error on the form if needed
-    //   if(newStatusCode !== 200){
+ 
 
-    //     // Set the formError field with the error text
-    //     document.querySelector("#"+formId+" .formError").innerHTML = 'Sorry, an error has occured. Please try again.';
 
-    //     // Show (unhide) the form error field on the form
-    //     document.querySelector("#"+formId+" .formError").style.display = 'block';
-
-    //   } else {
-    //     // If successful, set the token and redirect the user
-    //     app.setSessionToken(newResponsePayload);
-    //     window.location = '/checks/all';
-    //   }
-    // });
-
-    fetch("http://localhost:3000/api/token",{
+    fetch("http://localhost:3000/api/token", {
       method: 'POST',
-      headers:{
+      headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(newPayload)
     })
-    .then(res => res.json())
-    .then(responsePayload =>{ 
-      // if(typeof(requestPayload) === 'object'){
-            // If successful, set the token and redirect the user
+      .then(res => res.json())
+      .then(responsePayload => {
+        // if(typeof(requestPayload) === 'object'){
+        // If successful, set the token and redirect the user
         app.setSessionToken(responsePayload);
         // window.location = '/checks/all';
-      // }
-    })
+        // }
+      })
 
 
 
@@ -245,19 +240,19 @@ app.formResponseProcessor = function(formId,requestPayload){
 };
 
 // Get the session token from localstorage and set it in the app.config object
-app.getSessionToken = function(){
+app.getSessionToken = function () {
   var tokenString = localStorage.getItem('token');
-  if(typeof(tokenString) == 'string'){
-    try{
+  if (typeof (tokenString) == 'string') {
+    try {
       var token = JSON.parse(tokenString);
       console.log();
       app.config.sessionToken = token;
-      if(typeof(token) == 'object'){
+      if (typeof (token) == 'object') {
         app.setLoggedInClass(true);
       } else {
         app.setLoggedInClass(false);
       }
-    }catch(e){
+    } catch (e) {
       app.config.sessionToken = false;
       app.setLoggedInClass(false);
     }
@@ -265,9 +260,9 @@ app.getSessionToken = function(){
 };
 
 // Set (or remove) the loggedIn class from the body
-app.setLoggedInClass = function(add){
+app.setLoggedInClass = function (add) {
   var target = document.querySelector("body");
-  if(add){
+  if (add) {
     target.classList.add('loggedIn');
   } else {
     target.classList.remove('loggedIn');
@@ -275,11 +270,11 @@ app.setLoggedInClass = function(add){
 };
 
 // Set the session token in the app.config object as well as localstorage
-app.setSessionToken = function(token){
-  console.log(token,"from token");
+app.setSessionToken = function (token) {
+  console.log(token, "from token");
   var tokenString = JSON.stringify(token);
-  localStorage.setItem('token',tokenString);
-  if(typeof(token) == 'object'){
+  localStorage.setItem('token', tokenString);
+  if (typeof (token) == 'object') {
     app.setLoggedInClass(true);
   } else {
     app.setLoggedInClass(false);
@@ -287,76 +282,45 @@ app.setSessionToken = function(token){
 };
 
 // Renew the token
-app.renewToken = function(callback){
+app.renewToken = function (callback) {
   console.log("rew");
-  var currentToken = typeof(app.config.sessionToken) == 'object' ? app.config.sessionToken : false;
-  if(currentToken){
-  //   // Update the token with a new expiration
+  var currentToken = typeof (app.config.sessionToken) == 'object' ? app.config.sessionToken : false;
+  if (currentToken) {
+    //   // Update the token with a new expiration
     var payload = {
-      'id' : currentToken.id,
-      'extend' : true,
+      'id': currentToken.id,
+      'extend': true,
     };
-console.log("renew", payload );
-  //   app.client.request(undefined,'api/token','PUT',undefined,payload,function(statusCode,responsePayload){
-  //     // Display an error on the form if needed
-  // console.log( "From test", statusCode,responsePayload, );
-
-  //     if(statusCode == 200){
-  //       // Get the new token details
-       
-  //     } else {
-  //       app.setSessionToken(false);
-  //       callback(true);
-  //     }
-  //   });
-
-
-  fetch("http://localhost:3000/api/token",{
-    method: 'PUT',
-    headers:{
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(payload)
-  })
-  .then(res => res.json())
-  .then(responsePayload =>{ 
-    // if(typeof(requestPayload) === 'object'){
-          // If successful, set the token and redirect the user
-      console.log(responsePayload,"from update");
-      var queryStringObject = {'id' : currentToken.id};
-
-      fetch(`http://localhost:3000/api/token?id=${queryStringObject.id}`,{
-        method: 'GET',
-        headers:{
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
+    fetch("http://localhost:3000/api/token", {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
       .then(res => res.json())
-      .then(data =>{
-        if(typeof(data) === 'object'){
-          console.log(data,"from get method")
-          app.setSessionToken(data);
-        }else{
-               app.setSessionToken(false);
-        }
-      } )
-
-      // app.client.request(undefined,'api/tokene','GET',queryStringObject,undefined,function(statusCode,responsePayload){
-      //   // Display an error on the form if needed
-      //   console.log(statusCode, responsePayload, "from the ajx request");
-      //   if(statusCode == 200){
-
-      //     // app.setSessionToken(responsePayload);
-      //     // callback(false);
-      //   } else {
-      //     // app.setSessionToken(false);
-      //     // callback(true);
-      //   }
-      // });
-    // }
-  })
+      .then(responsePayload => {
+        console.log(responsePayload, "from update");
+        var queryStringObject = { 'id': currentToken.id };
+        // Get the renew token with the help of queryString
+        fetch(`http://localhost:3000/api/token?id=${queryStringObject.id}`, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(res => res.json())
+          .then(data => {
+            if (typeof (data) === 'object') {
+              console.log(data, "from get method")
+              app.setSessionToken(data);
+            } else {
+              app.setSessionToken(false);
+            }
+          })
+      })
 
 
 
@@ -364,23 +328,23 @@ console.log("renew", payload );
   } else {
     // app.setSessionToken(false);
     // callback(true);
-  
+
   }
 };
 
 // Loop to renew token often
-app.tokenRenewalLoop = function(){
-  setInterval(function(){
-    app.renewToken(function(err){
-      if(!err){
-        console.log("Token renewed successfully @ "+Date.now());
+app.tokenRenewalLoop = function () {
+  setInterval(function () {
+    app.renewToken(function (err) {
+      if (!err) {
+        console.log("Token renewed successfully @ " + Date.now());
       }
     });
-  },1000 * 60);
+  }, 1000 * 60);
 };
 
 // Init (bootstrapping)
-app.init = function(){
+app.init = function () {
 
   // Bind all form submissions
   app.bindForms();
@@ -394,6 +358,6 @@ app.init = function(){
 };
 
 // Call the init processes after the window loads
-window.onload = function(){
+window.onload = function () {
   app.init();
 };
